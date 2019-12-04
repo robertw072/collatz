0ed37d77371243f10342dc954069a8d40753c2fc
Robert P. Williamson <williamson_rp@cobra.cs.mercer.edu>
2019-12-04 13:45:40 -0500

    comments updated

--- ruby/recursed/recursedcollatz.rb
+++ ruby/recursed/recursedcollatz.rb
@@ -2,20 +2,20 @@
 
 Tuple = Struct.new(:num, :length)   # struct to hold integer/length pairs
 
-def count_collatz(n, count)    # function to compute the collatz length for a given n
-    if n == 1 then
+def count_collatz(n, count)    # recursive function to compute the collatz length for a given n
+    if n == 1 then             # if n is at 1, stop
         return count
     end
-    if n%2 == 1 then
+    if n%2 == 1 then           # if odd do this
         n = 3*n + 1
         return count_collatz(n, count+1)
-    else 
+    else                       # if even do this
         n = n / 2
         return count_collatz(n, count+1)
     end
 end 
 
-def sort_length(array)
+def sort_length(array)  # bubblesort to sort by length
     n = array.length
     loop do
       swapped = false
@@ -33,7 +33,7 @@ def sort_length(array)
     array
 end
 
-def sort_magnitude(array)
+def sort_magnitude(array)   # bubble sort for sorting by magnitude
     n = array.length
     loop do
       swapped = false
@@ -52,7 +52,7 @@ def sort_magnitude(array)
 end
 
 n = 1                       # holds value to be computed
-count = 0
+count = 0                   # count is the length of collatz sequence
 collatz_list = Array.new    # array to store integer/length pairs
 
 for i in 0..5000000000-1
@@ -61,9 +61,10 @@ for i in 0..5000000000-1
     tup.length = count_collatz(n, count)
     collatz_list[i] = tup
     n = n + 1
+    count = 0;              # reset count for next call to count_collatz
 end
 
-sort_length(collatz_list)
+sort_length(collatz_list)   # sort by length
 
 puts "The 10 longest collatz sequence lengths sorted by length: "
 small = Array.new
@@ -72,7 +73,7 @@ for i in 0..9
     small[i] = collatz_list[i]
 end
 
-sort_magnitude(small)
+sort_magnitude(small)       #sort by magnitude
 
 puts "The 10 longest collatz sequence lengths sorted by magnitude: "
 for i in 0..9
