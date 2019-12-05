c4dd9158c6364a9a1e386cb84118adcac84a90c9
Robert P. Williamson <williamson_rp@cobra.cs.mercer.edu>
2019-12-05 14:37:12 -0500

    recursive python fixed

--- python/recursed/reursedcollatz.py
+++ python/recursed/reursedcollatz.py
@@ -11,25 +11,36 @@ def countCollatz(n, count):
         n = n / 2
         return countCollatz(n, count+1)
 
-collatzList = []    # declare the list to hold the integer/length pairs of the sequence
+num = []    # parallel arrays used to hold integer n and the collatz sequence length
+length = []
+# zero the list
+for i in range(10):
+    num.append(0)
+    length.append(0)
+
 n = 1               # value to compute the length of the collatz sequence for
-for i in range(5000000000):
-    count = 0                   # count reset for each recursive function call
-    tup1 = (n, countCollatz(n, count))  # integer/length pair
-    collatzList.append(tup1)
+max_value = 5000000000   # collatz values to compute length of
+for i in xrange(max_value):
+    count = 0
+    result = countCollatz(n, count)    # length of collatz sequence for n
+    if (result > length[9]):    # determine if result is a max
+        offset = 0
+        while (result < length[offset]):    # determine where result should go
+            offset = offset + 1
+
+        # adjust the positions of the other values in lists
+        temp = offset
+        offset = 9
+        while (offset > temp):
+            num[offset] = num[offset - 1]
+            length[offset] = length[offset - 1]
+            offset = offset - 1
+        # put numbers in list
+        num[temp] = n
+        length[temp] = result    
     n = n + 1                   # increment the integer to be computed
-    
-collatzList.sort(key=lambda tup: tup[1], reverse=True)  # sort the list by length of collatz sequence
 
-small = []          # declare the list to hold the 10 largest collatz lengths
-
+# print the list
 print("The 10 longest Collatz Sequence lengths sorted by length: ")
 for i in range (9): # print the list of the 10 largest collatz seqences
-    print(collatzList[i])
-    small.append(collatzList[i])                        # store the 10 largest collatz sequence lengths to easily sort by magnitude
-
-small.sort(key= lambda tup: tup[0], reverse=True)       # sort by magnitude
-
-print("The 10 longest Collatz Sequence lengths sorted by magnitude: ")
-for i in range (9):
-    print(small[i])
+    print("n: %s length: %s" % (num[i], length[i]))
